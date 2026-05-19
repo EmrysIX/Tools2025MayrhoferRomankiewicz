168fc9709f1d39c7a334f5b1e5542c2ae6910cdb
Nicole Mayrhofer <s2510237918@students.fh.hagenberg.at>
2026-05-19 12:16:40 +0200

    first method added

---
+++ mayrhofer-nicole_annaromankiewicz/test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+    // einfache Methode
+    public void hello() {
+        System.out.println("Hallo Welt!");
+    }
+
+    // main-Methode zum Ausführen
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.hello();
+    }
+}
